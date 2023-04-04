37bb2b1989d95886b194f48d5c258f00ec514e8b
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-03 22:59:24 -0400

    testing file upload functions

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -80,6 +80,8 @@ func TestTools_UploadFiles(t *testing.T) {
 		var testTools Tools
 
 		testTools.AllowedFileTypes = tt.allowedTypes
+
+		// test UploadFiles function
 		uploadedFiles, err := testTools.UploadFiles(request, "./testdata/uploads/", tt.renameFile)
 		if err != nil && !tt.errorExpected {
 			t.Errorf("Error uploading file: %v", err)
@@ -99,6 +101,82 @@ func TestTools_UploadFiles(t *testing.T) {
 		}
 
 		wg.Wait()
+
+	}
+}
+
+func TestTools_UploadOneFile(t *testing.T) {
+	var uploadTests = []struct {
+		name          string
+		allowedTypes  []string
+		renameFile    bool
+		errorExpected bool
+	}{
+		{"allowed no rename", []string{"image/jpeg", "image/png"}, false, false},
+		{"allowed rename", []string{"image/jpeg", "image/png"}, true, false},
+		{"not allowed", []string{"image/png"}, false, true},
+		{"not allowed rename", []string{"image/png"}, true, true},
+	}
+
+	for _, tt := range uploadTests {
+		// setup a pipe to avoid buffering
+		pr, pw := io.Pipe()
+		// create a new multipart writer
+		mw := multipart.NewWriter(pw)
+
+		go func() {
+			defer mw.Close()
+
+			// create a new form data filed for the file
+			part, err := mw.CreateFormFile("file", "./testdata/nyc.jpeg")
+			if err != nil {
+				t.Errorf("Error creating form file: %v", err)
+			}
+
+			file, err := os.Open("./testdata/nyc.jpeg")
+			if err != nil {
+				t.Errorf("Error opening file: %v", err)
+			}
+			defer file.Close()
+
+			img, _, err := image.Decode(file)
+			if err != nil {
+				t.Errorf("Error decoding image: %v", err)
+			}
+
+			err = jpeg.Encode(part, img, nil)
+			if err != nil {
+				t.Errorf("Error encoding image: %v", err)
+			}
+
+		}()
+
+		// read from the pipe which receives the multipart data
+		request := httptest.NewRequest("POST", "/", pr)
+		request.Header.Add("Content-Type", mw.FormDataContentType())
+
+		var testTools Tools
+
+		testTools.AllowedFileTypes = tt.allowedTypes
+
+		// test UploadOneFile function
+		uploadedFiles, err := testTools.UploadOneFile(request, "./testdata/uploads/", tt.renameFile)
+		if err != nil && !tt.errorExpected {
+			t.Errorf("Error uploading file: %v", err)
+		}
+
+		if !tt.errorExpected {
+			if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName)); os.IsNotExist(err) {
+				t.Errorf("File does not exist on the destination directory: %s on test %s", err.Error(), tt.name)
+			}
+
+			// remove the file
+			_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName))
+		}
+
+		if tt.errorExpected && err == nil {
+			t.Errorf("%s: Error was expected but none received", tt.name)
+		}
 
 	}
 }
