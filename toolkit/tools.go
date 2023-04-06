1e7551c52119adb64a83e4197276938de86bc302
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-06 19:09:21 -0400

    read json function

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -2,6 +2,7 @@ package toolkit
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -17,8 +18,10 @@ const randonStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 // Tools is a toolkit for general purpose
 type Tools struct {
-	MaxFileSize      int64
-	AllowedFileTypes []string
+	MaxFileSize        int64
+	AllowedFileTypes   []string
+	MaxJSONSize        int
+	AllowUnknownFields bool
 }
 
 // RandomString returns a random string of length n
@@ -201,5 +204,42 @@ func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, osPat
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
 
 	http.ServeFile(w, r, filePath)
+
+}
+
+// JSONResponse is a struct to hold JSON response
+type JSONResponse struct {
+	Error bool   `json:"error"`
+	Msg   string `json:"msg"`
+	Data  any    `json:"data,omitempty"`
+}
+
+// ReadJSON reads a JSON request body
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+
+	maxBytes := 1024 * 1024
+	if t.MaxJSONSize != 0 {
+		maxBytes = t.MaxJSONSize
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("request body must only contain a single JSON object")
+	}
+
+	return nil
 
 }
