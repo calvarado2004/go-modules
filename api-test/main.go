355fee28a8419f8985df2dfac0eb554ecb66f5c9
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-08 23:40:27 -0400

    dummy api finalized

--- api-test/main.go
+++ api-test/main.go
@@ -18,16 +18,51 @@ func main() {
 	}
 }
 
+// routes returns a new ServeMux
 func routes() *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	mux.HandleFunc("/api/login", login)
 	mux.HandleFunc("/api/logout", logout)
 
 	return mux
 }
 
+// login and logout handlers
 func login(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("login"))
+
+	var tools toolkit.Tools
+
+	var payload struct {
+		Email    string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	err := tools.ReadJSON(w, r, &payload)
+	if err != nil {
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	var resPayload toolkit.JSONResponse
+
+	if payload.Email == "me@here.com" && payload.Password == "password" {
+		resPayload = toolkit.JSONResponse{
+			Msg: "Logged in",
+		}
+		_ = tools.WriteJSON(w, http.StatusAccepted, resPayload)
+		return
+	} else {
+		resPayload = toolkit.JSONResponse{
+			Msg:   "Invalid login",
+			Error: true,
+		}
+		_ = tools.WriteJSON(w, http.StatusUnauthorized, resPayload)
+		return
+	}
+
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
